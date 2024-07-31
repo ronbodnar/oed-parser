90571962bc32305e6069024d853c20c2f11d8c8f
Ron Bodnar <ron.bodnar@outlook.com>
2024-07-31 02:10:11 -0700

    Changed file names, added a few other scripts for processing the data and inserting into a MySQL database.

--- src/convert-data.py
+++ src/convert-data.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import csv
 import json
 import re
@@ -87,6 +88,20 @@ def populate_words(input_file):
     IOError: If there is an issue with reading the input file.
     """
     
+=======
+import os
+import csv
+from word import Word
+
+# Input file path.
+input_file = os.path.join(os.path.dirname(__file__), 'parsed-data.txt')
+
+# The list to house Word objects.
+words = {}
+
+# Read all records in the input file and create Word objects from the data.
+def populate_words():
+>>>>>>> be8b0d6 (Changed words data structure in convert-data.py to a dictionary to efficiently ignore duplicate words. Added barebones functions for writing output to csv and raw words to a txt file.)
     with open(input_file, encoding="utf-8") as file:
         reader = csv.reader(file)
         
@@ -94,6 +109,7 @@ def populate_words(input_file):
         for row in reader:
             # Skip bad rows for now.
             if len(row) != 4:
+<<<<<<< HEAD
                 print(f"Bad row length for row {row}")
                 continue
             
@@ -127,6 +143,18 @@ def populate_words(input_file):
                         
             # Instantiate a new Word object with the data and join the speech parts with a | delimiter.
             word = Word(text, snippet, "|".join(speech_parts))
+=======
+                continue
+            
+            page_num, text, snippet, parts_of_speech = row
+            
+            # Skips duplicate words (should probably merge snippets or handle them however you'd like).
+            if text in words:
+                continue
+            
+            # Instantiate a new Word object with the data.
+            word = Word(text, snippet, parts_of_speech)
+>>>>>>> be8b0d6 (Changed words data structure in convert-data.py to a dictionary to efficiently ignore duplicate words. Added barebones functions for writing output to csv and raw words to a txt file.)
             
             # And add it to the words dictionary.
             words[text] = word
@@ -136,6 +164,7 @@ def populate_words(input_file):
     
     # Find all Words without parts of speech.
     no_parts_of_speech = [word for word in words if words[word].parts_of_speech == '']
+<<<<<<< HEAD
     
     # Output our findings.
     print(f"Found {len(words)} words, {len(no_snippet)} without snippets, {len(no_parts_of_speech)} without parts of speech")
@@ -218,3 +247,21 @@ def write_json(file, minified):
     
 if __name__ == "__main__":
     main()
+=======
+    print(f"Found {len(words)} words, {len(no_snippet)} without snippets, {len(no_parts_of_speech)} without parts of speech")
+    
+def write_csv():
+    with open(os.path.join(os.path.dirname(__file__), 'out.csv'), 'w', encoding="utf-8") as file:
+        writer = csv.writer(file)
+        
+        for word in words:
+            writer.writerow([words[word].text, words[word].snippet, words[word].parts_of_speech])
+            
+def write_words():
+    with open(os.path.join(os.path.dirname(__file__), 'words.txt'), 'w', encoding="utf-8") as file:
+        file.writelines([words[word].text + '\n' for word in words])
+
+populate_words()
+write_csv()
+write_words()
+>>>>>>> be8b0d6 (Changed words data structure in convert-data.py to a dictionary to efficiently ignore duplicate words. Added barebones functions for writing output to csv and raw words to a txt file.)
